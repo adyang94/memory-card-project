0d7d6e6c69f3083564fa3a9bb23d0466897ca118
adyang94 <adyang94@yahoo.com>
2021-04-03 10:55:21 -0500

    adding photos

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,21 @@
 import React, {useEffect, useState} from 'react';
+import img1 from '../pictures/1.jpg';
+import img2 from '../pictures/2.jpg';
 
 function Game (props) {
-  const pictureInfo = {
-    
-  }
+  
+  
+  let pictureInfo = [
+    {
+      name: "img1",
+      src: require("../pictures/1.jpg")
+    },
+    {
+      name: "img2",
+      src: require("../pictures/2.jpg")
+    }
+  ];
+
   const [score, setScore] = useState(0);
 
   const increment = () => {
@@ -36,6 +48,17 @@ function Game (props) {
       >
         Save
       </button>
+      <button>Randomize</button>
+      <br/>
+      <img src = {img1} alt ="Dog 1" height = "100px" width = "100px" />
+      <img src = {img2} alt = "Dog 2" height = "100px" width = "100px" />
+
+      {pictureInfo.map((pictureInfo, i) => {
+        
+        return (
+          <img src = {pictureInfo.src} key={i} alt="" height = "100px" width = "100px" />
+        );
+      })}
     </div>
   )
 }
