57a79e277990e408e5bac7656c412383e60b57b4
adyang94 <adyang94@yahoo.com>
2021-04-03 15:56:29 -0500

    starting tracker

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import img1 from '../pictures/1.jpg';
 import img2 from '../pictures/2.jpg';
+import img3 from '../pictures/3.jpg';
+import img4 from '../pictures/4.jpg';
+import img5 from '../pictures/5.jpg';
+import img6 from '../pictures/6.jpg';
+import img7 from '../pictures/7.jpg';
+import img8 from '../pictures/8.jpg';
 
 function Game (props) {
-  const [imgArrayOrdering, setImgArryOrdering] = useState([0,1])
+  const [imgArrayOrdering, setImgArrayOrdering] = useState([0, 1, 2, 3, 4, 5, 6, 7])
   
   let pictureInfo = [
     {
@@ -13,10 +19,47 @@ function Game (props) {
     {
       name: "img2",
       src: img2
+    },
+    {
+      name: "img3",
+      src: img3
+    },
+    {
+      name: "img4",
+      src: img4
+    },
+    {
+      name: "img5",
+      src: img5
+    },
+    {
+      name: "img6",
+      src: img6
+    },
+    {
+      name: "img7",
+      src: img7
+    },
+    {
+      name: "img8",
+      src: img8
     }
   ];
 
   const randomizer = () => {
+    console.log('RANDOMIZER RUNNING');
+    let newArrayOrdering = [];
+    let newNumber = Math.floor(Math.random() * 8);
+
+    while (newArrayOrdering.length !== 8) {
+      while (newArrayOrdering.includes(newNumber)) {
+        newNumber = Math.floor(Math.random() * 8);
+      }
+
+      newArrayOrdering.push(newNumber);
+      console.log(`New Number: ${newNumber}`);
+    }
+    setImgArrayOrdering(newArrayOrdering)
 
   };
 
@@ -54,13 +97,13 @@ function Game (props) {
       >
         Save
       </button>
-      <button>Randomize</button>
+      <button onClick = {randomizer}>Randomize</button>
       <br/>
       
 
       {imgArrayOrdering.map((index) => {
         return(
-          <img src={pictureInfo[index].src} alt="" key={index} height = "100px" width = "100px" />
+          <img src={pictureInfo[index].src} alt="" key={index} height = "100px" width = "100px" onClick = {randomizer} />
         )
       })}
 
